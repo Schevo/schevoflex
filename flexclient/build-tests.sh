4067dd2f5ab3284fb30683a0b58da3b2a20f7313
Matthew Scott <gldnspud@gmail.com>
2008-02-11 18:02:07 -0800

    Use proper dir for Flash/Flex libs

--- flexclient/build-tests.sh
+++ flexclient/build-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../../flex3sdk_b3_121207/bin/mxmlc -compiler.library-path ../../flexunit/bin ../../flex3sdk_b3_121207/lib/ -sp src -sp tests -- tests/SchevoTestRunner.mxml
+../../flex3sdk_b3_121207/bin/mxmlc -compiler.library-path ../../flexunit/bin ../../flex3sdk_b3_121207/frameworks/libs/ -sp src -sp tests -- tests/SchevoTestRunner.mxml
