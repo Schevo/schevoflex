e040057401a1851607d44a0d367a6bfdb2ecf39d
Matthew Scott <gldnspud@gmail.com>
2008-02-11 18:13:30 -0800

    Compiles cleanly now

--- flexclient/build-tests.sh
+++ flexclient/build-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../../flex3sdk_b3_121207/bin/mxmlc -compiler.library-path ../../flexunit/bin ../../flex3sdk_b3_121207/frameworks/libs/ -sp src -sp tests -- tests/SchevoTestRunner.mxml
+../../flex3sdk_b3_121207/bin/mxmlc -library-path+=../../flexunit/bin,../../flex3sdk_b3_121207/frameworks/libs/ -source-path+=src,tests -- tests/SchevoTestRunner.mxml
