e5867a5de498e9741494a2e93d897f649646b350
Matthew Scott <gldnspud@gmail.com>
2008-02-12 17:25:22 -0800

    Do not include in tests

--- testflexclient.py
+++ testflexclient.py
@@ -8,20 +8,22 @@ import setup_config as cfg
 
 TEST_RUNNER_SWF = join(cfg.BASE_DIR, 'SchevoTestRunner.swf')
 
-result = call([
-    cfg.MXMLC_EXE,
-    '-library-path+=%s' % join(cfg.FLEXUNIT, 'bin'),
-    '-source-path+=%s,%s' % (join(cfg.FLEXCLIENT_DIR, 'src'),
-                             join(cfg.FLEXCLIENT_DIR, 'tests')),
-    '-output=%s' % TEST_RUNNER_SWF,
-    '--',
-    join(cfg.FLEXCLIENT_DIR, 'tests', 'SchevoTestRunner.mxml'),
-    ])
 
-if result != 0:
-    exit(result)
+if __name__ == '__main__':
+    result = call([
+        cfg.MXMLC_EXE,
+        '-library-path+=%s' % join(cfg.FLEXUNIT, 'bin'),
+        '-source-path+=%s,%s' % (join(cfg.FLEXCLIENT_DIR, 'src'),
+                                 join(cfg.FLEXCLIENT_DIR, 'tests')),
+        '-output=%s' % TEST_RUNNER_SWF,
+        '--',
+        join(cfg.FLEXCLIENT_DIR, 'tests', 'SchevoTestRunner.mxml'),
+        ])
 
-call([
-    cfg.FLASH_PLAYER_EXE,
-    TEST_RUNNER_SWF,
-    ])
+    if result != 0:
+        exit(result)
+
+    call([
+        cfg.FLASH_PLAYER_EXE,
+        TEST_RUNNER_SWF,
+        ])
